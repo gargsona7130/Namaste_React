a17f1ef6a5c999cfec9d4aa369d91ef57c818529
gargsona7130 <gargsonal7130@gmail.com>
2024-05-28 13:12:33 +0530

    Second commit

--- App.js
+++ App.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-const heading = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
-    "heading"
-  );
-  const heading1 = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
-    "heading1"
-  );
-  
-  const container = React.createElement(
-    "div",
-    {
-      id: "container",
-    },
-    [heading, heading1]
-  );
-  // create root using createRoot
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  // passing react element inside root
-  root.render(container);
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading =(
+<h1 id="heading" key="heading"> Namaste Sonal This is JSX </h1>
+);
+
+
+const Title = () => {
+    return(
+        <h2 id="title" key="title"> Namaste Title </h2>
+    )
+}
+
+//note yha function likh diya to no arrow function
+
+const header = function(){
+    return(
+        <div>
+            <Title/>
+
+            <h1>Namaste React Functional component</h1>
+    <h2>This is h2 tag</h2>
+
+        </div>
+          )
+        }
+        
+        const root= ReactDOM.createRoot(document.getElementById("root"));
+        root.render(<header/>);
