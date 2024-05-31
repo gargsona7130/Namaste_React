6f5a0540cd5a728e0cc81dbf7ef60d478fba08a7
gargsona7130 <gargsonal7130@gmail.com>
2024-05-31 12:47:03 +0530

    component file

---
+++ Components/Footer.js
@@ -0,0 +1,9 @@
+// Footer component for footer section
+const Footer = () => {
+    const year = new Date().getFullYear();
+    return (
+        <h4> Footer </h4>
+    );
+  };
+  
+  export default Footer;
